3dab5ac19aae1b2d0549b482cd1a5b430d7db261
Jason Hancock <jason@graymeta.com>
2019-06-26 16:32:27 -0700

    remove cheekybits/is

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -3,11 +3,10 @@ package logger
 import (
 	"bytes"
 	"fmt"
-	"strings"
 	"testing"
 	"time"
 
-	"github.com/cheekybits/is"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLogger(t *testing.T) {
@@ -16,51 +15,47 @@ func TestLogger(t *testing.T) {
 	l := New(&buf, "somelogger", "info", "key1", "value1")
 
 	t.Run("info", func(t *testing.T) {
-		is := is.New(t)
 		defer buf.Reset()
 
 		l.Info("foo", "key2", "value2")
 
-		is.True(strings.Contains(buf.String(), "key1=value1"))
-		is.True(strings.Contains(buf.String(), "key2=value2"))
-		is.True(strings.Contains(buf.String(), "caller=github.com/graymeta/saas/internal/logger/logger_test.go"))
-		is.True(strings.Contains(buf.String(), "ts="+fmt.Sprintf("%d", time.Now().Year())))
-		is.True(strings.Contains(buf.String(), "src=somelogger"))
-		is.True(strings.Contains(buf.String(), "level=info"))
-		is.True(strings.Contains(buf.String(), "msg=foo"))
+		require.Contains(t, buf.String(), "key1=value1")
+		require.Contains(t, buf.String(), "key2=value2")
+		require.Contains(t, buf.String(), "caller=github.com/graymeta/gmkit/logger/logger_test.go")
+		require.Contains(t, buf.String(), "ts="+fmt.Sprintf("%d", time.Now().Year()))
+		require.Contains(t, buf.String(), "src=somelogger")
+		require.Contains(t, buf.String(), "level=info")
+		require.Contains(t, buf.String(), "msg=foo")
 
 	})
 
 	t.Run("check-level-filtering", func(t *testing.T) {
-		is := is.New(t)
 		defer buf.Reset()
 
 		l.Debug("debug_message", "keyDebug", "valueDebug")
-		is.False(strings.Contains(buf.String(), "debug_message"))
+		require.NotContains(t, buf.String(), "debug_message")
 	})
 
 	t.Run("sub-logger", func(t *testing.T) {
-		is := is.New(t)
 		defer buf.Reset()
 
 		sub := l.New("sublogger")
 		sub.Info("sub", "key3", "value3")
 
-		is.True(strings.Contains(buf.String(), "key1=value1"))
-		is.True(strings.Contains(buf.String(), "key3=value3"))
-		is.True(strings.Contains(buf.String(), "src=somelogger.sublogger"))
+		require.Contains(t, buf.String(), "key1=value1")
+		require.Contains(t, buf.String(), "key3=value3")
+		require.Contains(t, buf.String(), "src=somelogger.sublogger")
 	})
 
 	t.Run("sub-log-with-vals", func(t *testing.T) {
-		is := is.New(t)
 		defer buf.Reset()
 
 		sub := l.New("sublogger2").With("key4", "value4")
 		sub.Info("sub", "key5", "value5")
 
-		is.True(strings.Contains(buf.String(), "key1=value1"))
-		is.True(strings.Contains(buf.String(), "key4=value4"))
-		is.True(strings.Contains(buf.String(), "key5=value5"))
-		is.True(strings.Contains(buf.String(), "src=somelogger.sublogger2"))
+		require.Contains(t, buf.String(), "key1=value1")
+		require.Contains(t, buf.String(), "key4=value4")
+		require.Contains(t, buf.String(), "key5=value5")
+		require.Contains(t, buf.String(), "src=somelogger.sublogger2")
 	})
 }
