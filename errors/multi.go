f3b9350823aac219d5b728f606c21ed70cd87d51
Jonathan Tobe <jonathan.tobe@graymeta.com>
2019-11-13 12:11:35 -0800

    Adding multi-error support.

---
+++ errors/multi.go
@@ -0,0 +1,88 @@
+package errors
+
+import (
+	"errors"
+	"strings"
+)
+
+// multiErr is a container for multiple errors which preserves our gmkit/errors
+// error behaviors.
+type multiErr struct {
+	errs []error
+
+	conflict  bool
+	exists    bool
+	notFound  bool
+	retry     bool
+	temporary bool
+}
+
+// Append adds a new error to an existing (possibly nil) error.
+func Append(old, new error) error {
+	if old == nil {
+		return nil
+	}
+
+	var merr *multiErr
+	if !errors.As(old, &merr) {
+		merr = &multiErr{}
+		merr = Append(merr, old).(*multiErr)
+	}
+
+	if new == nil {
+		return merr
+	}
+
+	merr.errs = append(merr.errs, new)
+	var cerr Conflicter
+	if errors.As(new, &cerr) {
+		merr.conflict = true
+	}
+	var exerr Exister
+	if errors.As(new, &exerr) {
+		merr.exists = true
+	}
+	var nferr NotFounder
+	if errors.As(new, &nferr) {
+		merr.notFound = true
+	}
+	var rerr Retrier
+	if errors.As(new, &rerr) {
+		merr.retry = true
+	}
+	var terr Temporarier
+	if errors.As(new, &terr) {
+		merr.temporary = true
+	}
+
+	return merr
+}
+
+func (m multiErr) Error() string {
+	var errs []string
+	for i := range m.errs {
+		errs = append(errs, m.errs[i].Error())
+	}
+
+	return strings.Join(errs, ": ")
+}
+
+func (m *multiErr) Conflict() bool {
+	return m.conflict
+}
+
+func (m *multiErr) Exists() bool {
+	return m.exists
+}
+
+func (m *multiErr) NotFound() bool {
+	return m.notFound
+}
+
+func (m *multiErr) Retry() bool {
+	return m.retry
+}
+
+func (m *multiErr) Temporary() bool {
+	return m.temporary
+}
---
+++ errors/multi_test.go
@@ -0,0 +1,167 @@
+package errors_test
+
+import (
+	"errors"
+	"testing"
+
+	gmerrors "github.com/graymeta/gmkit/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMulti(t *testing.T) {
+	t.Run("handles nil old error", func(t *testing.T) {
+		err := gmerrors.Append(nil, errors.New("something wrong"))
+		require.NoError(t, err)
+	})
+
+	t.Run("handles nil new error", func(t *testing.T) {
+		err := gmerrors.Append(errors.New("something wrong"), nil)
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "something wrong")
+	})
+
+	t.Run("handles nil old and new errors", func(t *testing.T) {
+		err := gmerrors.Append(nil, nil)
+		require.NoError(t, err)
+	})
+
+	t.Run("existing non-multi conflict error", func(t *testing.T) {
+		old := &testConflictErr{}
+		err := gmerrors.Append(old, errors.New("something wrong"))
+		require.Error(t, err)
+		require.Implements(t, (*gmerrors.Conflicter)(nil), err)
+		assert.True(t, err.(gmerrors.Conflicter).Conflict())
+		assert.Contains(t, err.Error(), "something wrong")
+	})
+
+	t.Run("existing non-multi exists error", func(t *testing.T) {
+		old := &testExistsErr{}
+		err := gmerrors.Append(old, errors.New("something wrong"))
+		require.Error(t, err)
+		require.Implements(t, (*gmerrors.Exister)(nil), err)
+		assert.True(t, err.(gmerrors.Exister).Exists())
+		assert.Contains(t, err.Error(), "something wrong")
+	})
+
+	t.Run("existing non-multi notFound error", func(t *testing.T) {
+		old := &testNotFoundErr{}
+		err := gmerrors.Append(old, errors.New("something wrong"))
+		require.Error(t, err)
+		require.Implements(t, (*gmerrors.NotFounder)(nil), err)
+		assert.True(t, err.(gmerrors.NotFounder).NotFound())
+		assert.Contains(t, err.Error(), "something wrong")
+	})
+
+	t.Run("existing non-multi retry error", func(t *testing.T) {
+		old := &testRetryErr{}
+		err := gmerrors.Append(old, errors.New("something wrong"))
+		require.Error(t, err)
+		require.Implements(t, (*gmerrors.Retrier)(nil), err)
+		assert.True(t, err.(gmerrors.Retrier).Retry())
+		assert.Contains(t, err.Error(), "something wrong")
+	})
+
+	t.Run("existing non-multi temporary error", func(t *testing.T) {
+		old := &testTemporaryErr{}
+		err := gmerrors.Append(old, errors.New("something wrong"))
+		require.Error(t, err)
+		require.Implements(t, (*gmerrors.Temporarier)(nil), err)
+		assert.True(t, err.(gmerrors.Temporarier).Temporary())
+		assert.Contains(t, err.Error(), "something wrong")
+	})
+
+	t.Run("append conflict error", func(t *testing.T) {
+		new := &testConflictErr{}
+		err := gmerrors.Append(errors.New("something wrong"), new)
+		require.Error(t, err)
+		require.Implements(t, (*gmerrors.Conflicter)(nil), err)
+		assert.True(t, err.(gmerrors.Conflicter).Conflict())
+		assert.Contains(t, err.Error(), "something wrong")
+	})
+
+	t.Run("append exists error", func(t *testing.T) {
+		new := &testExistsErr{}
+		err := gmerrors.Append(errors.New("something wrong"), new)
+		require.Error(t, err)
+		require.Implements(t, (*gmerrors.Exister)(nil), err)
+		assert.True(t, err.(gmerrors.Exister).Exists())
+		assert.Contains(t, err.Error(), "something wrong")
+	})
+
+	t.Run("append notFound error", func(t *testing.T) {
+		new := &testNotFoundErr{}
+		err := gmerrors.Append(errors.New("something wrong"), new)
+		require.Error(t, err)
+		require.Implements(t, (*gmerrors.NotFounder)(nil), err)
+		assert.True(t, err.(gmerrors.NotFounder).NotFound())
+		assert.Contains(t, err.Error(), "something wrong")
+	})
+
+	t.Run("append retry error", func(t *testing.T) {
+		new := &testRetryErr{}
+		err := gmerrors.Append(errors.New("something wrong"), new)
+		require.Error(t, err)
+		require.Implements(t, (*gmerrors.Retrier)(nil), err)
+		assert.True(t, err.(gmerrors.Retrier).Retry())
+	})
+
+	t.Run("append temporary error", func(t *testing.T) {
+		new := &testTemporaryErr{}
+		err := gmerrors.Append(errors.New("something wrong"), new)
+		require.Error(t, err)
+		require.Implements(t, (*gmerrors.Temporarier)(nil), err)
+		assert.True(t, err.(gmerrors.Temporarier).Temporary())
+		assert.Contains(t, err.Error(), "something wrong")
+	})
+}
+
+type testConflictErr struct{}
+
+func (err *testConflictErr) Error() string {
+	return "conflict"
+}
+
+func (err *testConflictErr) Conflict() bool {
+	return true
+}
+
+type testExistsErr struct{}
+
+func (err *testExistsErr) Error() string {
+	return "exists"
+}
+
+func (err *testExistsErr) Exists() bool {
+	return true
+}
+
+type testNotFoundErr struct{}
+
+func (err *testNotFoundErr) Error() string {
+	return "not found"
+}
+
+func (err *testNotFoundErr) NotFound() bool {
+	return true
+}
+
+type testRetryErr struct{}
+
+func (err *testRetryErr) Error() string {
+	return "retry me!"
+}
+
+func (err *testRetryErr) Retry() bool {
+	return true
+}
+
+type testTemporaryErr struct{}
+
+func (err *testTemporaryErr) Error() string {
+	return "retry me!"
+}
+
+func (err *testTemporaryErr) Temporary() bool {
+	return true
+}
